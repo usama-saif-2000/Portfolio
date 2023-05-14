f27902ca32df33844f039d12c3f2fe09805e5f64
usama-saif-2000 <osamabinsaif2000@gmai.com>
2023-05-14 14:59:46 +0500

    resume button with responnsiveness

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
     <div className="app-container">
       <MainPage />
       <AboutMe />
+      
       <Projects />
       <Contact />
     </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,24 +7,24 @@ export default function Navbar() {
   return (
     <div className="nav-section">
       <div className="nav-container">
-        <a href="#home">
-          <div className="nav-logo">{name}</div>
-        </a>
-        <img src={menu} className="hamburger-menu" />
-        <ul>
-          <li>
-            <a href="#home">Home</a>
-          </li>
-          <li>
-            <a href="#aboutme">About me</a>
-          </li>
-          <li>
-            <a href="#projects">Projects</a>
-          </li>
-          <li>
-            <a href="#contact">Contact</a>
-          </li>
-        </ul>
+          <a href="#home">
+            <div className="nav-logo">{name}</div>
+          </a>
+          <img src={menu} className="hamburger-menu" />
+          <ul>
+            <li>
+              <a href="#home">Home</a>
+            </li>
+            <li>
+              <a href="#aboutme">About me</a>
+            </li>
+            <li>
+              <a href="#projects">Projects</a>
+            </li>
+            <li>
+              <a href="#contact">Contact</a>
+            </li>
+          </ul>
       </div>
     </div>
   );
